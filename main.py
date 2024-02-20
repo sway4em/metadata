10936385beb4f2b2a83e95680568a4a7004d5000
Swayam Chidrawar <schidraw@calpoly.edu>
2024-02-19 17:58:48 -0800

    working on streamlit

---
+++ main.py
@@ -0,0 +1,37 @@
+import boto3
+import json
+
+def main():
+    bedrock_runtime = boto3.client(
+        service_name="bedrock-runtime",
+        region_name="us-west-2"
+    )
+    context = ""
+    prompt = "Your prompt here."
+    kwargs = {
+        "modelId": "anthropic.claude-v2:1",
+        "contentType": "application/json",
+        "accept": "*/*",
+        "body": json.dumps({
+            "prompt": "\n\nHuman:For our conversation talk like Shakespeare. " + context + prompt + "\n\nAssistant:",
+            "max_tokens_to_sample": 300,
+            "temperature": 0.5,
+            "top_k": 250,
+            "top_p": 1,
+            "stop_sequences": ["\n\nHuman:"],
+            "anthropic_version": "bedrock-2023-05-31"
+        }).encode('utf-8')
+    }
+    response = bedrock_runtime.invoke_model_with_response_stream(**kwargs)
+    stream = response.get('body')
+    if stream:
+        for event in stream:
+            chunk = event.get('chunk')
+            if chunk:
+                response_text = json.loads(chunk.get('bytes')).get('completion')
+                print(response_text, end="")
+                context += prompt + response_text
+    print('\n\n')
+
+if __name__ == '__main__':
+    main()
